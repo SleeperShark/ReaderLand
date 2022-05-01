5dfbc5ea1e15ce99b67df008e387620502b5e5a7
EC2 Default User <ec2-user@ip-172-31-4-79.ap-northeast-1.compute.internal>
2022-05-01 04:14:49 +0000

    FIX: port

--- server.js
+++ server.js
@@ -3,15 +3,15 @@ require('dotenv').config();
 if (process.env.NODE_ENV == 'production') {
     const app = require('./app');
     const mongoose = require('mongoose');
-    const { MONGO_USER, MONGO_PASSWORD, MONGO_DATABASE, port } = process.env;
+    const { MONGO_USER, MONGO_PASSWORD, MONGO_DATABASE, PORT } = process.env;
     const connectURL = `mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@readerland.uebkf.mongodb.net/${MONGO_DATABASE}?retryWrites=true&w=majority`;
 
     mongoose
         .connect(connectURL)
         .then(() => {
             console.log('MongoDB connected...');
-            app.listen(port, () => {
-                console.log(`Server runing on port ${port}`);
+            app.listen(PORT, () => {
+                console.log(`Server runing on port ${PORT}`);
             });
         })
         .catch((error) => {
